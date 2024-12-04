96dfd5610e2e66fdf12bf0056a3d3ab6ef83c281
Dicky Pratama <prtmaars@gmail.com>
2024-12-04 13:36:39 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -184,7 +184,7 @@ def main():
     st.title("Bike Sharing Dashboard")
     
     # Load Data
-    df = load_data("C:/Users/prtma/OneDrive/Documents/Project Dicoding/data_hrfix.csv")
+    df = load_data("https://github.com/prtmaars/Bike-Sharing-DC/blob/ddfdbf67d055713bb0991f137ad1fcbd14c697ed/dashboard/data_hrfix.csv")
     
     # Apply Filters
     filtered_df = apply_filters(df)
